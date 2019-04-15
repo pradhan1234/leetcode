e7628eec015c0e55e057cba8dca6461db82140d6
pradhanyash <yash_pradhan@hotmail.com>
2019-04-14 17:28:59 -0700

    update

--- 0055. Jump Game/Solution.java
+++ 0055. Jump Game/Solution.java
@@ -20,7 +20,7 @@
 
 class Solution {
     public boolean canJump(int[] nums) {
-        // we'll try reverse engineering, can we reach to index 0 from index n-1.
+        // start from last index, can we reach to index 0 from index n-1.
         int cursor = nums.length - 1;
         for (int i = nums.length - 2; i >= 0; i--) {
             // if standing at index i, with max jump nums[i] can we go past cursor?
