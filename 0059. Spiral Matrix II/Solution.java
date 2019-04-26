d57fceef45c9c950fd1d7b588a8b558c18a0e126
pradhanyash <yash_pradhan@hotmail.com>
2019-04-25 23:47:31 -0700

    clean 21 - 100

--- 0059. Spiral Matrix II/Solution.java
+++ 0059. Spiral Matrix II/Solution.java
@@ -1,18 +1,3 @@
-/***
- * Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
- *
- * Example:
- *
- * Input: 3
- * Output:
- * [
- *  [ 1, 2, 3 ],
- *  [ 8, 9, 4 ],
- *  [ 7, 6, 5 ]
- * ]
- */
-
-
 class Solution {
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
