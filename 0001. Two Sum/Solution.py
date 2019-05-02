b3ed840c58f1d830370af43ca6c06cf823cf5c82
Darshil Parikh <darshil.parikh@gmail.com>
2019-05-01 23:42:57 -0500

    Added Comments for better understanding

--- 0001. Two Sum/Solution.py
+++ 0001. Two Sum/Solution.py
@@ -1,3 +1,5 @@
+# Space Complexity : O(n)
+# Time Complexity : O(n)
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -8,8 +10,10 @@ class Solution:
         if len(nums) < 1:
             return False
         ad_dict = {}
+	# we would store complement of a number and its index(because we want to return index)
         for i in range(len(nums)):
             if target - nums[i] in ad_dict:
+		# We have found the target - current number in the dictionary and hence we return both the indices
                 return [ad_dict[target - nums[i]], i]
             else:
                 ad_dict[nums[i]] = i
