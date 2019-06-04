b4cbc079d10ca9ed4998a912138ee2b51c97d30e
Yash Pradhan <yash_pradhan@hotmail.com>
2019-06-03 23:31:37 -0700

    added space time complexities

--- 409. Longest Palindrome/Solution.java
+++ 409. Longest Palindrome/Solution.java
@@ -3,6 +3,10 @@
  * find the length of the longest palindromes that can be built with those letters.
  *
  * This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * 
+ * Time Complexity: O(n), where n is the length of the input string
+ * Space Complexity: O(s), where s is the size of symbols
+ * >> this can be considered to be constant, |a-zA-Z| = constant
  */
 
 class Solution {
@@ -21,7 +25,7 @@ class Solution {
                 set.add(c);
             }
         }
-
+        // if there are characters left in set, we consume one of them.
         if(set.isEmpty()) {
             return count;
         }
