b110dc0d9351c2a2eb50699550f02f20933899d4
Yash Pradhan <yash_pradhan@hotmail.com>
2019-06-12 23:31:29 -0700

    simplify solution

--- 0206. Reverse Linked List/recursive.java
+++ 0206. Reverse Linked List/recursive.java
@@ -7,41 +7,24 @@
  * }
  */
 class Solution {
-    
-    ListNode _head;
-    
     public ListNode reverseList(ListNode head) {
-        // setting up
-        ListNode cursor = head;
-        _head = head;
-        
-        // call to recursive function
-        reverseHelper(cursor);
-        
-        // return reversed linked list
-        return _head;
-    }
-    
-    private ListNode reverseHelper(ListNode cursor) {
-                
-        // base conditions
-        if(cursor == null) {
-            return cursor;
+        // base case: LL null or single node.
+        if(head == null || head.next == null) {
+            return head;
         }
-        
-        // this indicates last node, we set _head to it
-        if(cursor.next == null) {
-            _head = cursor;
-            return cursor;
-        }
-        
-        // now we call reverse on cursor.next,
-        // rev(1): 1 --> 2 --> 3 --> 4 --> 5 --> NULL
-        // cursor = 1; rev(2): 2 --> 3 --> 4 --> 5 --> NULL
-        // cursor = 1; _head = 5 --> 4 --> 3 --> 2 --> NULL; function returns reference to the last node in this reversed list, we set it's next to cursor
-        (reverseHelper(cursor.next)).next = cursor;
-        cursor.next = null;
-        
-        return cursor;
+        // Example:
+        // head --> 1 --> 2 --> 3 --> 4 --> NULL
+
+        // holds rest of the reversed list.
+        ListNode reversed = reverseList(head.next);
+        // head --> 1 ------->-------
+        //                          |
+        // reversed --> 4 --> 3 --> 2 --> NULL
+        //
+        // pointer rearrangement:
+        head.next.next = head;
+        head.next = null;
+        // reversed --> 4 --> 3 --> 2 --> 1 --> NULL
+        return reversed;
     }
 }
