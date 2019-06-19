94f863c3c7222f25176d9594c021b8d6a1cdfea1
Yash Pradhan <yash_pradhan@hotmail.com>
2019-06-18 23:39:10 -0700

    added comments

--- 0128. Longest Consecutive Sequence/Solution.java
+++ 0128. Longest Consecutive Sequence/Solution.java
@@ -1,9 +1,19 @@
+// Space Complexity: O(n)
+// Time Complexity: O(n)
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // The idea used here is pretty simple to understand.
+        // Before diving into that, we have a simple O(nlgn) sorting based solution for the problem.
+        // Sort the array, traverse it to find the longest consecutive sequence.
+        
+        // Let's talk about a better approach
         Set<Integer> set = new HashSet<>();
         for(int n: nums) {
             set.add(n);
         }
+        // key point: the streak start from smallest element that belongs to that streak
+        // there we only proceed iff element n-1 is not present, which implies n is the smallest element.
+        // keep track of current and global lengths.
         int longestLength = 0;
         int currentLength = 0;
         int currentValue;
@@ -18,7 +28,6 @@ class Solution {
                 longestLength = Math.max(longestLength, currentLength);
             }
         }
-
         return longestLength;
     }
 }
