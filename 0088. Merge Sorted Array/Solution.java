84ee081941b8c810c73a8ff2145bec0d72e0a00c
Tushar Gonawala <tushargonawala96@gmail.com>
2019-07-08 15:46:48 -0500

    Update Solution.java

--- 0088. Merge Sorted Array/Solution.java
+++ 0088. Merge Sorted Array/Solution.java
@@ -5,7 +5,7 @@
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //Start from the end, looking for filling the array in descending order.
+        //Start from the end, looking for filling the array in reverse order.
         while(m > 0 && n > 0){
             if(nums1[m-1] > nums2[n-1]){
                 nums1[m+n-1] = nums1[m-1];
