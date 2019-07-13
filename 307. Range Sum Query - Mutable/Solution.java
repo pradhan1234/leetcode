d0f72273b3f1899bbe6b8d56eef99b3086e25476
Yash Pradhan <yash_pradhan@hotmail.com>
2019-07-12 19:39:51 -0700

    added more comments

--- 307. Range Sum Query - Mutable/Solution.java
+++ 307. Range Sum Query - Mutable/Solution.java
@@ -45,18 +45,29 @@ class NumArray {
         buildSegmentTree(nums, 0, 0, n-1); 
     }
     
+    // we build segment tree recursively
+    // nums: original array
+    // index: represents an element in segment tree structure
+    // startIndex, endIndex: represent range of nums, of current call
     private int buildSegmentTree(int[] nums, int index, int startIndex, int endIndex) {
+        // base case
+        // when nums[startIndex .. endIndex] correspond to single element
+        // create a node for it in segmentTree at index
         if(startIndex == endIndex) {
             segmentTree[index] = nums[startIndex];
             return segmentTree[index];
         }
-        
+        // recursive building step
         int mid = startIndex + (endIndex - startIndex)/2;
+        // the node at segment tree at index, represents the range under it
+        // which is sum of both its children.
+        // modify start/end indices and current index and make recursive call
         segmentTree[index] = buildSegmentTree(nums, 2*index+1, startIndex, mid) 
                                  + buildSegmentTree(nums, 2*index+2, mid+1, endIndex); 
         return segmentTree[index];
     }
     
+    // print array
     private void print(int[] a) {
         System.out.print("Elements: ");
         for(int i = 0; i < a.length; i++) {
@@ -65,46 +76,65 @@ class NumArray {
         System.out.println();
     }
     
+    // to update element at index i in original array to val
     public void update(int i, int val) {
+        // check i in range?
         if( i < 0 || i >= n) {
             return;
         }
-        
-        int delta = val - nums_backup[i];
-        nums_backup[i] = val;
+
+        int delta = val - nums_backup[i]; // diff
+        nums_backup[i] = val; // update our backup copy
+        // update the segment tree
         updateSegmentTree(0, n-1, i, delta, 0);   
     }
     
+    // we update the segment tree recursively
+    // startIndex, endIndex, affectedIndex correspond to original array
+    // index corresponds to index in segment tree, for current call
     private void updateSegmentTree(int startIndex, int endIndex, int affectedIndex, int delta, int index) {
-    
+        // check affectedIndex in range?
         if(affectedIndex < startIndex || affectedIndex > endIndex) {
             return;
         }
-        
+        // update current node of segment tree with delta
         segmentTree[index] += delta;
-        if(startIndex<endIndex) {
+        // recursively update segment tree
+        if(startIndex < endIndex) {
             int mid = startIndex + (endIndex-startIndex)/2;
             updateSegmentTree(startIndex, mid, affectedIndex, delta, 2*index+1);
             updateSegmentTree(mid+1, endIndex, affectedIndex, delta, 2*index+2);
         }
     }
     
+    // returns sum range of arr[i .. j]
     public int sumRange(int i, int j) {
+        // check validity
         if (i < 0 || j > n - 1 || i > j) {
             return 0;
         }
         return querySegmentTree(0, n-1, i, j, 0);
     }
     
+    // we query the segment tree recursively
+    // startIndex, endIndex corresponds to indices in original array
+    // qStartIndex, qEndIndex corresponds to query indices in original array
+    // index corresponds to node in segment tree of current call
     private int querySegmentTree(int startIndex, int endIndex, int qStartIndex, int qEndIndex, int index) {
+        // We have 3 cases here to take care of:
+        // 1. Exact Match
+        // 2. No Overlap
+        // 3. Partial Overlap
+        
+        // Exact Match: return the value
         if(qStartIndex <= startIndex && qEndIndex >= endIndex) {
             return segmentTree[index];
         }
-        
+        // No Overlap: return 0
         if(startIndex > qEndIndex || endIndex < qStartIndex) {
             return 0;
         }
-        
+        // propagate recursively
         int mid = startIndex + (endIndex - startIndex)/2;
         return querySegmentTree(startIndex, mid, qStartIndex, qEndIndex, 2*index+1) 
                 + querySegmentTree(mid+1, endIndex, qStartIndex, qEndIndex, 2*index+2);
